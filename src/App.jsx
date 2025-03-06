0f6560f5222299b95b4cb3d595a3813a6cee2d92
Ankit sharma <ankitsharma0318@gmail.com>
2025-03-06 15:31:47 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
 import { useState } from "react";
 
 import "./App.css";
-
+const initialContacts = [
+  { id: 0, name: "Aarav", email: "aarav@mail.com" },
+  { id: 1, name: "Vivaan", email: "vivaan@mail.com" },
+  { id: 2, name: "Aditya", email: "aditya@mail.com" },
+  { id: 3, name: "Vihaan", email: "vihaan@mail.com" },
+  { id: 4, name: "Arjun", email: "arjun@mail.com" },
+  { id: 5, name: "Sai", email: "sai@mail.com" },
+  { id: 6, name: "Reyansh", email: "reyansh@mail.com" },
+  { id: 7, name: "Ayaan", email: "ayaan@mail.com" },
+  { id: 8, name: "Krishna", email: "krishna@mail.com" },
+  { id: 9, name: "Ishaan", email: "ishaan@mail.com" },
+];
+function EmailSys({name,email}) {
+  const [clicked, setClicked] = useState(false);
+  
+  return (
+    <div className="email-sys">
+      <h1>{name}</h1>
+      {clicked ? (
+        <h2>{email}</h2>
+      ) : (
+        <button onClick={() => setClicked((clicked) => !clicked)}>
+          show email
+        </button>
+      )}
+    </div>
+  );
+}
 function App() {
-  const [reciver, setReciver] = useState("muskaan");
-
+  const [contacts,setContacts]=useState(initialContacts);
+  const list=contacts.map((contact)=><EmailSys key={contact.id} name={contact.name} email={contact.email}/>)
   return (
     <>
+      <label>
+        <input type="checkbox" onChange={() =>setContacts([...contacts].reverse())} /> Show in reverse order{" "}
+      </label>
       <div className="container">
-        <div>
-          <button onClick={() => setReciver("muskaan")}>muskaan</button>
-          <br />
-          <button onClick={() => setReciver("neelam")}>neelam</button>
-          <br />
-          <button onClick={() => setReciver("ankit")}>ankit</button>
-          <br />
-        </div>
-        <div>
-          <textarea
-            placeholder={"chat to " + reciver}
-          ></textarea>
-          <br />
-          <a href={`mailto:${reciver}@mail.com`}>
-            <button>send to {reciver}@mail.com </button>
-          </a>
-        </div>
+        {list}
       </div>
     </>
   );
