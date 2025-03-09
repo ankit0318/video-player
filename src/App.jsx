5d4278ac47ac9187e016adb364e3a5860ef979e0
ankit0318 <ankitsharma0318@gmail.com>
2025-03-09 13:51:38 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,24 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+function VideoPlayer({ src, isPlaying }) {
+  const ref = useRef(null);
+  useEffect(() => {
+    if (isPlaying) ref.current.play();
+    else ref.current.pause();
+  },[isPlaying]);
+
+  return <video ref={ref} src={src} loop className="video-player"></video>;
+}
 
 function App() {
-  const [now, setNow] = useState(null);
-  const startTime = useRef(null);
-  const intervalRef = useRef(null);
-  const logsRef = useRef([]);
-  let secondsPassed = 0;
-
-  if (startTime.current != null && now != null) {
-    secondsPassed = (now - startTime.current) / 1000;
-  }
-
-  function handleStart() {
-    startTime.current = Date.now();
-    setNow(Date.now());
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current);
-    }
-    intervalRef.current = setInterval(() => setNow(Date.now()), 10);
-  }
-
-  function handleStop() {
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-      logsRef.current.push(secondsPassed.toFixed(3));
-      setNow(null); 
-    }
-  }
-
-  return (
-    <>
-      <h1>time passed: {secondsPassed.toFixed(3)}</h1>
-      <button onClick={handleStart}>start</button>
-      <button onClick={handleStop}>stop</button>
-      <h2>Logs:</h2>
-      <ul className="logs">
-        {logsRef.current.map((log, index) => (
-          <li key={index}>{log} seconds</li>
-        ))}
-      </ul>
-    </>
-  );
+  const [isPlaying,setIsPlaying]=useState(false);
+  return <>
+    <VideoPlayer src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4" isPlaying={isPlaying}/>
+    <div className="button-container">
+      <button onClick={()=>setIsPlaying(true)}>Play</button>
+      <button onClick={()=>setIsPlaying(false)}>Pause</button>
+    </div>
+  </>;
 }
 
 export default App;
